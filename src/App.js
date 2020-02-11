6d7da313c8fb81554b5a44294f2bd3eaa9a93c59
Sophi-E <sophierithous@gmail.com>
2020-02-11 15:04:11 +0100

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.css';
+import Weather from './components/Weather';
+
+const api = {
+  key: ' 2e455788b7fe48076c1405fd6507ff97',
+  base: 'https://api.openweathermap.org/data/2.5/'
+};
 
 function App() {
-  return <div className='App'>hello world</div>;
+  return (
+    <div className='App'>
+      <main>
+        <div className='search-box'>
+          <input type='text' className='search-box' placeholder='Search...' />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
